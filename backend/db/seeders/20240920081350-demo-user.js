65a8a720000298090a099b5d67edfac89561a83f
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-10-15 15:28:13 -0700

    Merge from migration and seeders

--- backend/db/seeders/20240920081350-demo-user.js
+++ backend/db/seeders/20240920081350-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Wreck-it',
+        lastName: 'Ralph',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Imposter',
+        lastName: 'Syndrome',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Fake',
+        lastName: 'User',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
